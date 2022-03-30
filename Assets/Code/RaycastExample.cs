cb136fc36a8d6f9e71944151e93fbc1346dc6d7a
Maximiliam Rosén <karlsson.maximiliam@gmail.com>
2022-03-30 20:44:53 +0200

    Created a cleaner solution

--- Assets/Code/RaycastExample.cs
+++ Assets/Code/RaycastExample.cs
@@ -1,34 +1,38 @@
 // created by Maximiliam Rosén
 
 using UnityEngine;
+
 public class RaycastExample : MonoBehaviour
 {
     private void OnDrawGizmos()
     {
-        var debugBoxSize = Vector3.one * 0.1f;
-        // The total distance to check for the collider
-        const float distance = 5f;
-        // Here we create the ray with the starting position and direction
         var ray = new Ray(transform.position, transform.forward);
-        // If we hit anything continue
-        if (!Physics.Raycast(ray, out var entryHit, distance)) return;
-        Gizmos.color = Color.green;
-        Gizmos.DrawCube(ray.origin, debugBoxSize);
-        Gizmos.DrawLine(ray.origin, ray.origin + ray.direction * entryHit.distance);
-        // Here we create a new starting position from the opposite side of the collider
-        ray.origin = ray.GetPoint(distance);
-        Gizmos.color = Color.cyan;
-        Gizmos.DrawCube(ray.origin, debugBoxSize);
-        // here we we flip the direction so the raycast start from the opposite side of the collider and pointing inwards
-        ray.direction = -ray.direction;
-        // If we hit anything continue
-        if (!entryHit.collider.Raycast(ray, out var exitHit, distance)) return;
+        if (!Physics.Raycast(ray, out var entryHit, float.MaxValue)) return;
+        var exitPoint = entryHit.collider.GetExitPosition(entryHit.point, ray.direction, 1000f);
+
+        // --------------------------------------------------------------------------------------------- Ignore this ---------------------------------------------------------------------------------------------
         Gizmos.color = Color.red;
-        // We now have the entryHit and the exitHit of the collider
-        Gizmos.DrawCube(entryHit.point, debugBoxSize);
-        Gizmos.DrawCube(exitHit.point, debugBoxSize);
-        Gizmos.DrawLine(entryHit.point, exitHit.point);
-        Gizmos.color = Color.cyan;
-        Gizmos.DrawLine(ray.origin, exitHit.point);
+        Gizmos.DrawWireSphere(entryHit.point, 0.1f);
+        Gizmos.DrawWireSphere(exitPoint, 0.1f);
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(transform.position, entryHit.point);
+        Gizmos.DrawLine(new Ray(entryHit.point, ray.direction).GetPoint(1000f), exitPoint);
+        // --------------------------------------------------------------------------------------------- Ignore this ---------------------------------------------------------------------------------------------
+    }
+}
+
+// The implementation
+internal static class ColliderExtension
+{
+    public static Vector3 GetExitPosition(this Collider collider, Vector3 entryPoint, Vector3 direction, float checkDistance) => GetExitPosition(collider, new Ray(entryPoint, direction), checkDistance);
+    public static Vector3 GetExitPosition(this Collider collider, Ray ray, float checkDistance)
+    {
+        // Get a point x distance from the entryPoint
+        ray.origin = ray.GetPoint(checkDistance);
+        // Reverse the ray direction
+        ray.direction = -ray.direction;
+        // Call the raycast from the collider
+        collider.Raycast(ray, out var hit, float.MaxValue);
+        return hit.point;
     }
 }
